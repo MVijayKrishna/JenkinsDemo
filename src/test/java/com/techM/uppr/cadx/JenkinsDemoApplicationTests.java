f4d9e97c5c7d4fb20ed4d2c86f6652e5dea8e4f0
sachin123 <sachin123@192.168.1.35>
2021-06-18 20:07:12 +0530

    my first commit

---
+++ src/test/java/com/techM/uppr/cadx/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techM.uppr.cadx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
